d796442ae26365932698778e6dd9da284bbe4101
Ulia Varfolomeeva <uliagorbenko73@gmail.com>
2025-02-20 15:21:46 +0300

    new file

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.scripts.sample import welcome
+
+def main():
+
+    if __name__ == "__main__":
+        main()
+
+    name = welcome()
+
+    print('Find the greatest common divisor of given numbers.')
