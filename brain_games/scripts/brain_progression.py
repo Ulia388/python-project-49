7c464ffcacd9054d06b4f2fb0f38155c4fab5521
Ulia Varfolomeeva <uliagorbenko73@gmail.com>
2025-02-23 07:04:04 +0300

    new file

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,32 @@
+import random
+from brain_games.scripts.sample import welcome
+from brain_games.scripts.sample import generate_progression
+
+def main():
+
+    if __name__ == "__main__":
+        main()
+
+    name = welcome()
+
+
+    print('What number is missing in the progression?')
+
+    for _ in range(3):
+        
+        progression, number = generate_progression()
+        correct_answer = number
+        print("Question:", ' '.join(map(str, progression)))
+
+        answer = int(input('Your answer: '))
+
+        if answer == correct_answer:
+            print('Correct!')
+
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+        
+    print(f'Congratulations, {name}!')
+        
