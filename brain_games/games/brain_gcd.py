158bb4c463bb5b2fcb7ff7720d7bfb3ff9b4cfda
Юлия Варфоломеева <uliagorbenko73@gmail.com>
2025-03-02 19:54:06 +0400

    modified

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -2,31 +2,12 @@ import math
 import random
 
 from brain_games.scripts.main import welcome
+DESCRIPTION = "Find the greatest common divisor of given numbers."
 
+def get_question_and_answer():
 
-def main():
-
-    if __name__ == "__main__":
-        main()
-
-    name = welcome()
-
-    print("Find the greatest common divisor of given numbers.")
-
-    for _ in range(3):
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
-        correct_answer = math.gcd(num1, num2)
-        print(f"Question: {num1} {num2}")
-
-        answer = int(input("Your answer: "))
-
-        if answer == correct_answer:
-            print("Correct!")
-
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-            
-    print(f"Congratulations, {name}!")
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    question = f'{num1} {num2}'
+    correct_answer = str(math.gcd(num1, num2))
+    return question, correct_answer
