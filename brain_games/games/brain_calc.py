48441f9f0e4644c414efc809f2637b327c1c5127
Юлия Варфоломеева <uliagorbenko73@gmail.com>
2025-03-02 18:12:14 +0400

    modified

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,17 +1,14 @@
 import random
-from brain_games.scripts.main import welcome
-from brain_games.games.basic import conditions_game, random_expression
+DESCRIPTION = "What is the result of the expression?"
 
 
-def main():
+def get_question_and_answer():
 
-    if __name__ == "__main__":
-        main()
-
-    name = welcome()
-    print("What is the result of the expression?")
-
-    numbers = random_expression()
-    correct_answer = eval(numbers)
-
-    conditions_game(numbers, correct_answer)
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    operations = ["+", "-", "*"]
+    operation = random.choice(operations)
+    question = f"{num1} {operation} {num2}"
+    correct_answer = int(eval(question))
+    
+    return question, correct_answer
