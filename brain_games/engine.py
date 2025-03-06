89936c22f10d8ec2b3f2d971b8f563b75e365ec6
Юлия Варфоломеева <uliagorbenko73@gmail.com>
2025-03-06 18:27:13 +0400

    new file

---
+++ brain_games/engine.py
@@ -0,0 +1,30 @@
+import prompt
+
+from brain_games.games.scripts.cli import welcome
+
+
+def run_game(description, get_question_and_answer):
+    name = welcome()
+    print(description)
+
+    for _ in range(3):
+        question, correct_answer = get_question_and_answer()
+
+        print(f"Question: {question}")
+        answer = prompt.string("Your answer: ")
+
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(
+                f"'{answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+    
+
+
+
