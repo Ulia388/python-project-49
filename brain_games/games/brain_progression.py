2168e48517132fb50a5ce02566f53266fe9d1d46
Юлия Варфоломеева <uliagorbenko73@gmail.com>
2025-03-16 08:45:34 +0400

    modified

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -2,6 +2,9 @@ import numpy as np
 
 DESCRIPTION = "What number is missing in the progression?"
 LENGTH_OF_PROGRESSION = 10
+FIRST_NUMBER_BOUNDARY = 10
+START_OF_STEP = 1
+END_OF_STEP = 5
 
 
 def generate_progression(start, step):
@@ -11,8 +14,8 @@ def generate_progression(start, step):
 
 
 def get_question_and_answer():
-    first_number_of_progression = np.random.randint(0, 10)
-    step_of_progression = np.random.randint(1, 5)
+    first_number_of_progression = np.random.randint(0, FIRST_NUMBER_BOUNDARY)
+    step_of_progression = np.random.randint(START_OF_STEP, END_OF_STEP)
     hidden_element_index = np.random.randint(0, LENGTH_OF_PROGRESSION)
 
     progression = generate_progression(
